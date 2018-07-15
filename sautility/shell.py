23eb714211ac589f8543fa67e69664c155afb6b7
sabuaka <sabuaka-fx@hotmail.com>
2018-07-15 17:01:54 +0900

    [mod](ver1.7.0) Add getch of nonblock type to shell module.

--- sautility/shell.py
+++ sautility/shell.py
@@ -2,6 +2,9 @@
 '''Shell utility module'''
 import sys
 import os
+import tty
+import fcntl
+import termios
 import builtins as __builtin__
 
 
@@ -13,6 +16,9 @@ def print(*args, **kwargs):  # override pylint: disable-msg=W0622
 
 def isccbreak(key):
     """getchで取得したキーがCtrl+Cであるかを判定します"""
+    if key is None or len(key) <= 0:
+        return False
+
     CTRL_C = 3   # 可読性を優先 pylint: disable-msg=C0103
     if ord(key) == CTRL_C:
         return True
@@ -27,19 +33,39 @@ def clear():
 
 
 def getch():
-    """キー入力を待ち入力されたキーを返します（inputとは異なりエンターを待ちません）"""
-    if sys.platform == 'win32':
-        from msvcrt import getch as lcgetch
-    else:
-        def lcgetch():
-            '''Wait for key input and returns inputed key (bytes type)'''
-            import tty
-            import termios
-            fd = sys.stdin.fileno()  # 可読性を優先 pylint: disable-msg=C0103
-            old = termios.tcgetattr(fd)
-            try:
-                tty.setraw(fd)
-                return sys.stdin.read(1).encode('utf-8')
-            finally:
-                termios.tcsetattr(fd, termios.TCSADRAIN, old)
-    return lcgetch()
+    '''Wait for key input and returns inputed key (bytes type)'''
+    fd = sys.stdin.fileno()  # 可読性を優先 pylint: disable-msg=C0103
+    old = termios.tcgetattr(fd)
+    in_key = 0
+    try:
+        tty.setraw(fd)
+        in_key = sys.stdin.read(1).encode('utf-8')
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old)
+    return in_key
+
+
+def nb_getch():
+    '''(nonblock)Monitoring key input.'''
+    # get current preferences
+    fno = sys.stdin.fileno()
+    org_attr = termios.tcgetattr(fno)
+    org_fcntl = fcntl.fcntl(fno, fcntl.F_GETFL)
+
+    # Temporarily exclude "icanon" from attribute of shell.
+    attr = termios.tcgetattr(fno)
+    attr[3] &= ~termios.ICANON
+    termios.tcsetattr(fno, termios.TCSADRAIN, attr)
+
+    # Temporarily set stdin to "NONBLOCK".
+    fcntl.fcntl(fno, fcntl.F_SETFL, org_fcntl | os.O_NONBLOCK)
+
+    # Monitoring key input
+    in_key = 0
+    try:
+        in_key = sys.stdin.read(1).encode('utf-8')
+    finally:
+        # set back original preferences.
+        fcntl.fcntl(fno, fcntl.F_SETFL, org_fcntl)
+        termios.tcsetattr(fno, termios.TCSANOW, org_attr)
+    return in_key
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name='sautility',
     packages=['sautility'],
-    version='1.6.0',
+    version='1.7.0',
     description='',
     author='sabuaka',
     author_email='sabuaka-fx@hotmail.com',
